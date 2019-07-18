8f399aa8773f52e365ac67e0b2d5b5e8e10735d8
DongTian <707493342@qq.com>
2019-07-18 22:32:36 +0800

    打印出a的值

--- src/main/java/com/dongtian/gitTest/demo1.java
+++ src/main/java/com/dongtian/gitTest/demo1.java
@@ -2,6 +2,8 @@ package com.dongtian.gitTest;
 
 public class demo1 {
     public static void main(String[] args){
-        System.out.println("我要修改代码了");
+        int a = 10;
+        System.out.println(a);
+        
     }
 }
