a1c233e18fef21ed1503a79a060328eb80bcbac7
DongTian <707493342@qq.com>
2019-07-18 22:31:12 +0800

    测试修改代码

--- src/main/java/com/dongtian/gitTest/demo1.java
+++ src/main/java/com/dongtian/gitTest/demo1.java
@@ -2,6 +2,6 @@ package com.dongtian.gitTest;
 
 public class demo1 {
     public static void main(String[] args){
-        System.out.printf("我要写代码了");
+        System.out.println("我要修改代码了");
     }
 }
